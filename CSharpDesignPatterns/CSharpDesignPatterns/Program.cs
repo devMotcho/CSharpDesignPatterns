2ede049c52616fff31fc9be928983d14d7e7521f
devMotcho <code.mmd2@gmail.com>
2024-12-21 10:45:47 +0000

    First Commit

---
+++ CSharpDesignPatterns/CSharpDesignPatterns/Program.cs
@@ -0,0 +1,142 @@
+﻿
+namespace CSharpDesignPatterns
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product(string name, Color c, Size s)
+    {
+        public string Name = name;
+        public Color Color = c;
+        public Size Size = s;
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize
+            (IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                    yield return p;
+            }
+        }
+        public IEnumerable<Product> FilterByColor
+            (IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                    yield return p;
+            }
+        }
+        public IEnumerable<Product> FilterBySizeAndColor
+            (IEnumerable<Product> products, Color color, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color && p.Size == size)
+                    yield return p;
+            }
+        }
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification(Color color) : ISpecification<Product>
+    {
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+    public class SizeSpecification(Size size) : ISpecification<Product>
+    {
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+
+        }
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+    public class Demo
+    {
+        private static void Main(string[] args)
+        {
+
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = [apple, tree, house];
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green Products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine(p.Name);
+            };
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green Products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine(p.Name);
+            }
+
+
+            Console.WriteLine("Large blue items (new):");
+            foreach (var p in bf.Filter(products,
+                         new AndSpecification<Product>(
+                             new ColorSpecification(Color.Blue),
+                             new SizeSpecification(Size.Large)
+                         )))
+            {
+                Console.WriteLine($"- {p.Name} is blue and big!");
+            };
+
+
+
+        }
+    }
+}
---
+++ LiskovPrinciple/LiskovPrinciple/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LiskovPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+            
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)} : {Width}, {nameof(Height)} : {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set => base.Width = base.Height = value;
+        }
+
+        public override int Height
+        {
+            set => base.Width = base.Height = value;
+        }
+    }
+
+    public class Demo
+    {
+        public static int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine($"{rc} has Area {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
