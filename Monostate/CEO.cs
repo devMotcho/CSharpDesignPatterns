a0c7822a7323825a10d6201eeb273615679b0d47
devMotcho <code.mmd2@gmail.com>
2025-02-06 23:17:00 +0000

    Monostate Pattern

---
+++ Monostate/CEO.cs
@@ -0,0 +1,24 @@
+﻿namespace Monostate;
+
+public class CEO
+{
+    private static string name;
+    private static int age;
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public int Age
+    {
+        get => age;
+        set => age = value;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
+}
---
+++ Monostate/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using Monostate;
+
+var ceo = new CEO();
+ceo.Name = "Bernardo Fernandes";
+ceo.Age = 26;
+
+var ceo2 = new CEO();
+Console.WriteLine(ceo2);
